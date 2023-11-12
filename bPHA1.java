3b9a60262b2bdf93a64b8965ef6547e1d87a653f
Julius <117359909+sirjulzz@users.noreply.github.com>
2023-11-12 17:47:12 +0100

    Update and rename bPHA1 to bPHA1.java

---
+++ bPHA1.java
@@ -0,0 +1,12 @@
+// Generic array methods for bPHA1 in PGdP and examplatory exercises including solutions
+// <|=================================================================================|>
+
+
+// <|=============================== Exercise 1 ======================================|>
+
+// Write a method that will move all even numbers on the first half and odd numbers to
+// the second half in an integer array.
+
+public static int[] paritySort(int[] array) {
+  return new int[0];
+}
