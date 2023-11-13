0169b8aaaa797af52445a9c17ed5d4fe7d47f230
Julius <117359909+sirjulzz@users.noreply.github.com>
2023-11-13 22:24:23 +0100

    add paritySort

--- pgdp2324bPHA1/src/pgdp/bPHA1/bPHA1.java
+++ pgdp2324bPHA1/src/pgdp/bPHA1/bPHA1.java
@@ -1,8 +1,30 @@
 package pgdp.hello;
 
-public class HelloPenguins {
+public class bPHA1 {
+    
+    // <|=============================== Exercise 1 ===============================|>
+    // Sort the array such that even numbers are strictly separated from odd numbers
+    // with even numbers being moved into the first half of the array.
+    
+    public static int[] paritySort(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] % 2 != 0) {
+				for (int j = i + 1; j < array.length; j++) {
+					int temp;
+					if (array[j] % 2 == 0) {
+						temp = array[i];
+						array[i] = array[j];
+						array[j] = temp;
+					}
+				}
+			}
+		}
 
+		return array;
+	}
+    
     public static void main(String[] args) {
-        System.out.println("Hallo Pinguine!");
+        int[] testArray = {1, 2, 3, 4, 5, 8, 9};
+        System.out.println("Test case: " + Arrays.toString(paritySort(testArray)));
     }
 }
